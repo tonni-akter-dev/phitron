1145e7283ef560bb39c2c7039728de1da7bac8e1
tonni-akter-dev <tanniakhter123@gmail.com>
2025-04-13 01:07:10 +0600

    module 5 done

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+ int x ,y;
+ scxnf("%d %d",&x,&y);
+
+printf("%d + %d = %d\n",x,y,x+y);
+printf("%d * %d = %d\n",x,y,x*y);
+printf("%d - %d = %d\n",x,y,x-y);
+ return 0;
+}
